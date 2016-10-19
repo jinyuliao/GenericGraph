f86d2e0a221cb5cb3a52fca54508171f68bd676f
jinyuliao <jinyuliao282@gmail.com>
2016-10-19 23:03:13 +0800

    remove unused code

--- Source/GenericGraphEditor/Private/GenericGraphFactory.cpp
+++ Source/GenericGraphEditor/Private/GenericGraphFactory.cpp
@@ -18,8 +18,6 @@ UGenericGraphFactory::~UGenericGraphFactory()
 UObject* UGenericGraphFactory::FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn)
 {
 	return NewObject<UObject>(InParent, Class, Name, Flags | RF_Transactional);
-// 	UDepGenericGraph* NewObjectAsset = ConstructObject(Class, InParent, Name, Flags | RF_Transactional);
-// 	return NewObjectAsset;
 }
 
 #undef LOCTEXT_NAMESPACE
