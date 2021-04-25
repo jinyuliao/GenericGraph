cd33f2f15908c4de76e07253161e4ed33e748351
sclark39 <sclark@freolic.com>
2021-04-25 14:00:13 -0400

    Make it so the edges can have names too. Add checkbox to display name in graph instead of the icon

--- Source/GenericGraphEditor/Public/GenericGraphAssetEditor/SEdNode_GenericGraphEdge.h
+++ Source/GenericGraphEditor/Public/GenericGraphAssetEditor/SEdNode_GenericGraphEdge.h
@@ -26,9 +26,15 @@ public:
 	// Calculate position for multiple nodes to be placed between a start and end point, by providing this nodes index and max expected nodes 
 	void PositionBetweenTwoNodesWithOffset(const FGeometry& StartGeom, const FGeometry& EndGeom, int32 NodeIndex, int32 MaxNodes) const;
 
+	void OnNameTextCommited(const FText& InText, ETextCommit::Type CommitInfo);
+
 protected:
 	FSlateColor GetEdgeColor() const;
 
+	const FSlateBrush* GetEdgeImage() const;
+
+	EVisibility GetEdgeImageVisibility() const;
+	EVisibility GetEdgeTitleVisbility() const;
 private:
 	TSharedPtr<STextEntryPopup> TextEntryWidget;
 };
