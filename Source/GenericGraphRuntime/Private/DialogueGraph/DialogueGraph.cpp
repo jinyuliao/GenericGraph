d852601579400bae01d97fbc6ad82efb818e7b44
Alex <kokotkin.alexander@gmail.com>
2024-01-01 17:59:46 +0200

    Implement Dialogue Graph

---
+++ Source/GenericGraphRuntime/Private/DialogueGraph/DialogueGraph.cpp
@@ -0,0 +1,18 @@
+﻿#include "DialogueGraph/DialogueGraph.h"
+#include "DialogueGraph/DialogueGraphNode.h"
+#include "DialogueGraph/DialogueGraphEdge.h"
+
+#define LOCTEXT_NAMESPACE "DialogueGraph"
+
+UDialogueGraph::UDialogueGraph()
+{
+    NodeType = UDialogueGraphNode::StaticClass();
+    EdgeType = UDialogueGraphEdge::StaticClass();
+
+    NPCDialogueNodeColor = FLinearColor::Red;
+    PlayerDialogueNodeColor = FLinearColor::Green;
+    
+    Name = "DialogueGraph";
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/GenericGraphRuntime/Public/DialogueGraph/DialogueGraph.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GenericGraph.h"
+#include "DialogueGraph.generated.h"
+
+UCLASS(Blueprintable)
+class GENERICGRAPHRUNTIME_API UDialogueGraph : public UGenericGraph
+{
+    GENERATED_BODY()
+
+public:
+    UDialogueGraph();
+
+    UPROPERTY(EditDefaultsOnly, Category = "DialogueGraph")
+    FLinearColor NPCDialogueNodeColor;
+
+    UPROPERTY(EditDefaultsOnly, Category = "DialogueGraph")
+    FLinearColor PlayerDialogueNodeColor;
+};
