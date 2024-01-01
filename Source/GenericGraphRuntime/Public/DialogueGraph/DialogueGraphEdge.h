bd7dc229cd7de687de27564e6a114aefc1d32850
Alex <kokotkin.alexander@gmail.com>
2024-01-01 18:00:13 +0200

    Implement Dialogue Graph Edge

---
+++ Source/GenericGraphRuntime/Public/DialogueGraph/DialogueGraphEdge.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GenericGraphEdge.h"
+#include "DialogueGraphEdge.generated.h"
+
+UCLASS(Blueprintable)
+class GENERICGRAPHRUNTIME_API UDialogueGraphEdge : public UGenericGraphEdge
+{
+    GENERATED_BODY()
+};
