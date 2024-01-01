173adfe03fdd596ace3502d83970fc50e209a7e9
Alex <kokotkin.alexander@gmail.com>
2024-01-01 18:01:38 +0200

    Implement Factory for Dialogue Graph

---
+++ Source/GenericGraphEditor/Private/DialogueGraph/DialogueGraphFactory.cpp
@@ -0,0 +1,29 @@
+﻿#include "DialogueGraph/DialogueGraphFactory.h"
+#include "DialogueGraph/DialogueGraph.h"
+
+#define LOCTEXT_NAMESPACE "DialogueSessionFactory"
+
+UDialogueGraphFactory::UDialogueGraphFactory()
+{
+    bCreateNew = true;
+    bEditAfterNew = true;
+    SupportedClass = UDialogueGraph::StaticClass();
+}
+
+UObject* UDialogueGraphFactory::FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context,
+    FFeedbackContext* Warn)
+{
+    return  NewObject<UObject>(InParent, Class, Name, Flags | RF_Transactional);
+}
+
+FText UDialogueGraphFactory::GetDisplayName() const
+{
+    return LOCTEXT("FactoryName", "Dialogue Graph");
+}
+
+FString UDialogueGraphFactory::GetDefaultNewAssetName() const
+{
+    return "DialogueGraph";
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/GenericGraphEditor/Public/DialogueGraph/DialogueGraphFactory.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Factories/Factory.h"
+#include "DialogueGraphFactory.generated.h"
+
+UCLASS()
+class UDialogueGraphFactory : public UFactory
+{
+    GENERATED_BODY()
+public:
+    UDialogueGraphFactory();
+
+    virtual UObject* FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn) override;
+    virtual FText GetDisplayName() const override;
+    virtual FString GetDefaultNewAssetName() const override;
+};
